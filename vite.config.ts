f9e0429a88c6e1a651060ab16c4a52353af4fecb
Lobinda <lobinda123@gmail.com>
2025-01-08 16:21:33 +0800

    init

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/<REPO_NAME>/", // 假設你的 repo 名稱叫做 my-app，就改成 '/my-app/'
+  base: "/reactHW2025/",
 });
